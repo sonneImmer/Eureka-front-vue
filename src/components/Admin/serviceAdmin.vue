e8a1b71676a7d80fdb1582bc38b1927dea7d0c07
linyi <linyi@>
2020-11-06 09:50:10 +0800

    修改了serviceAdmin

--- src/components/Admin/serviceAdmin.vue
+++ src/components/Admin/serviceAdmin.vue
@@ -1,58 +1,35 @@
 <template>
+  <el-container>
+    <el-aside>
+      <header-aside></header-aside>
+    </el-aside>
     <el-container>
-      <el-aside>
-        <header-aside></header-aside>
-      </el-aside>
-      <el-container>
-        <br>
-        <el-header>
+      <el-main>
+        <el-card :border="true" style="text-align:center">
+          <p style="font-size: 18px">注册中心状态</p>
           <el-row>
-          <el-col :span="4">
-            <div class="grid-content bg-purple">
-              <div class="sub-title">开发环境</div>
-              <el-autocomplete
-                class="inline-input"
-                v-model="state1"
-                :fetch-suggestions="querySearch"
-                placeholder="请输入内容"
-                @select="handleSelect"
-              ></el-autocomplete>
-            </div>
-          </el-col>
-          <el-col :span="4" offset="1">
+            <el-col :span="4">
+              <div class="grid-content bg-purple">
+                <div class="sub-title">开发环境</div>
+                <el-input></el-input>
+              </div>
+            </el-col>
+            <el-col :span="4" :offset="1">
               <div class="grid-content bg-purple">
                 <div class="sub-title">当前时刻</div>
-                <el-autocomplete
-                  class="inline-input"
-                  v-model="state1"
-                  :fetch-suggestions="querySearch"
-                  placeholder="请输入内容"
-                  @select="handleSelect"
-                ></el-autocomplete>
+                <el-input></el-input>
               </div>
             </el-col>
-          <el-col :span="4" offset="1">
+            <el-col :span="4" :offset="1">
               <div class="grid-content bg-purple">
                 <div class="sub-title">租赁到期使能</div>
-                <el-autocomplete
-                  class="inline-input"
-                  v-model="state1"
-                  :fetch-suggestions="querySearch"
-                  placeholder="请输入内容"
-                  @select="handleSelect"
-                ></el-autocomplete>
+                <el-input></el-input>
               </div>
             </el-col>
-          <el-col :span="4" offset="1">
+            <el-col :span="4" :offset="1">
               <div class="grid-content bg-purple">
                 <div class="sub-title">最后一分钟更新</div>
-                <el-autocomplete
-                  class="inline-input"
-                  v-model="state1"
-                  :fetch-suggestions="querySearch"
-                  placeholder="请输入内容"
-                  @select="handleSelect"
-                ></el-autocomplete>
+                <el-input></el-input>
               </div>
             </el-col>
           </el-row>
@@ -60,77 +37,69 @@
             <el-col :span="4">
               <div class="grid-content bg-purple">
                 <div class="sub-title">数据中心</div>
-                <el-autocomplete
-                  class="inline-input"
-                  v-model="state1"
-                  :fetch-suggestions="querySearch"
-                  placeholder="请输入内容"
-                  @select="handleSelect"
-                ></el-autocomplete>
+                <el-input></el-input>
               </div>
             </el-col>
-            <el-col :span="4" offset="1">
+            <el-col :span="4" :offset="1">
               <div class="grid-content bg-purple">
                 <div class="sub-title">开启时间</div>
-                <el-autocomplete
-                  class="inline-input"
-                  v-model="state1"
-                  :fetch-suggestions="querySearch"
-                  placeholder="请输入内容"
-                  @select="handleSelect"
-                ></el-autocomplete>
+                <el-input></el-input>
               </div>
             </el-col>
-            <el-col :span="4" offset="1">
+            <el-col :span="4" :offset="1">
               <div class="grid-content bg-purple">
                 <div class="sub-title">更新阙值（分钟）</div>
-                <el-autocomplete
-                  class="inline-input"
-                  v-model="state1"
-                  :fetch-suggestions="querySearch"
-                  placeholder="请输入内容"
-                  @select="handleSelect"
-                ></el-autocomplete>
+                <el-input></el-input>
               </div>
             </el-col>
           </el-row>
-        </el-header>
-        <hr>
-        <hr>
-        <el-main>
+        </el-card>
+      </el-main>
+      <el-main>
+        <el-card :border="true" style="text-align:center">
+          <p style="font-size: 18px">服务发现伙伴</p>
           <el-row class="demo-autocomplete">
-            <el-col :span="4">
+            <el-col :span="4" >
               <el-button>服务器1：地区一</el-button>
             </el-col>
-            <el-col :span="4">
+            <el-col :span="4" :offset="1">
               <el-button>服务器2：地区二</el-button>
             </el-col>
           </el-row>
-        </el-main>
-        <el-main>
+        </el-card>
+      </el-main>
+      <el-main>
+        <el-card :border="true">
+          <p style="font-size: 18px;text-align: center">Eureka已注册服务</p>
           <service-admin-table :dataList="serviceRequestData"></service-admin-table>
-        </el-main>
-        <el-main>
-            <el-table
-              :data="tableData"
-              height="250"
-              border
-              style="width: 100%;text-align: center;">
-              <el-table-column
-                prop="name"
-                label="合约名称"
-                width="200">
-              </el-table-column>
-              <el-table-column
-                prop="theme"
-                label="合约内容">
-              </el-table-column>
-            </el-table>
-        </el-main>
-      </el-container>
-      <!--    </div>-->
-      <!--  </div>-->
+        </el-card>
+      </el-main>
+      <el-main>
+        <el-card :border="true">
+        <p style="font-size: 18px;text-align: center">合约模板</p>
+        <el-table
+          :data="tableData"
+          height="250"
+          border
+          style="width: 100%;text-align: center;">
+          <el-table-column
+            prop="name"
+            label="合约名称"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="theme"
+            label="合约内容"
+            width="600">
+          </el-table-column>
+        </el-table>
+        </el-card>
+      </el-main>
+      <el-footer></el-footer>
     </el-container>
+    <!--    </div>-->
+    <!--  </div>-->
+  </el-container>
 
 </template>
 
@@ -159,9 +128,7 @@ export default {
           name:'服务合同合同',
           theme:'Term...'
         },
-
       ]
-
     }
   },
   mounted () {
@@ -182,9 +149,7 @@ export default {
 </script>
 
 <style scoped>
-
-  .el-aside{
-    background-color: #545c64;
-  }
-
+.el-aside{
+  background-color: #545c64;
+}
 </style>
